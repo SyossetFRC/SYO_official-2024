e8e8135f16ac5e9225b7dd69885d4c8d7d70e743
Darius Fan <fandar@syossetschools.org>
2023-05-17 17:26:24 -0400

    Changed from Joystick to Controller

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -14,14 +14,14 @@ public class RobotContainer {
     private final DrivetrainSubsystem m_drivetrainSubsystem = new DrivetrainSubsystem();
     
     private final Joystick m_driveController = new Joystick(0);
-    private double m_rotatePower;
+    private double m_powerLimit = 1.0;
 
     public RobotContainer() {
       m_drivetrainSubsystem.setDefaultCommand(new DefaultDriveCommand(
           m_drivetrainSubsystem,
-          () -> -MathUtil.applyDeadband(m_driveController.getRawAxis(1), 0.05) * (-m_driveController.getRawAxis(3) + 1) * DrivetrainSubsystem.kMaxSpeed,
-          () -> -MathUtil.applyDeadband(m_driveController.getRawAxis(0), 0.05) * (-m_driveController.getRawAxis(3) + 1) * DrivetrainSubsystem.kMaxSpeed,
-          () -> m_rotatePower * (-m_driveController.getRawAxis(3) + 1) * DrivetrainSubsystem.kMaxAngularSpeed
+          () -> -MathUtil.applyDeadband(m_driveController.getRawAxis(1), 0.05) * m_powerLimit * DrivetrainSubsystem.kMaxSpeed,
+          () -> -MathUtil.applyDeadband(m_driveController.getRawAxis(0), 0.05) * m_powerLimit * DrivetrainSubsystem.kMaxSpeed,
+          () -> (-MathUtil.applyDeadband(m_driveController.getRawAxis(4), 0.05) / 3.0) * m_powerLimit * DrivetrainSubsystem.kMaxAngularSpeed
       ));
 
       configureButtons();
@@ -35,35 +35,25 @@ public class RobotContainer {
     }
 
     private void configureButtons() {
-      Button m_resetPose = new Button(() -> m_driveController.getRawButton(6));
+      Button m_resetPose = new Button(() -> m_driveController.getRawButton(1));
       m_resetPose.whenPressed(() -> setPose(0, 0, 0));
 
-      Button m_brake = new Button(() -> m_driveController.getRawButton(10));
+      Button m_brake = new Button(() -> m_driveController.getRawButton(3));
       m_brake.whenPressed(new BrakeCommand(m_drivetrainSubsystem));
       m_brake.whenReleased(() -> m_drivetrainSubsystem.getCurrentCommand().cancel());
 
-      Button m_rotateLeft = new Button(() -> m_driveController.getRawButton(11));
-      m_rotateLeft.whenPressed(() -> setRotatePower("left"));
-      m_rotateLeft.whenReleased(() -> setRotatePower("none"));
+      Button m_incrementPowerLimit = new Button(() -> (m_driveController.getPOV() >= 315 || m_driveController.getPOV() <= 45));
+      m_incrementPowerLimit.whenPressed(() -> changePowerLimit(0.1));
 
-      Button m_rotateRight = new Button(() -> m_driveController.getRawButton(12));
-      m_rotateRight.whenPressed(() -> setRotatePower("right"));
-      m_rotateRight.whenReleased(() -> setRotatePower("none"));
+      Button m_decrementPowerLimit = new Button(() -> (m_driveController.getPOV() >= 135 && m_driveController.getPOV() <= 225));
+      m_decrementPowerLimit.whenPressed(() -> changePowerLimit(-0.1));
     }
 
     public void setPose(double xPos, double yPos, double theta) { m_drivetrainSubsystem.setPose(xPos, yPos, theta); }
 
     public void setIdleMode(String idleMode) { m_drivetrainSubsystem.setIdleMode(idleMode); }
 
-    private void setRotatePower(String state) {
-      if (state.equals("left")) {
-        m_rotatePower = 0.2;
-      }
-      else if (state.equals("right")) {
-        m_rotatePower = -0.2;
-      }
-      else {
-        m_rotatePower = 0;
-      }
+    private void changePowerLimit(double delta) { 
+      if ((m_powerLimit <= 1.0 - Math.abs(delta) || delta <= 0) && (m_powerLimit >= Math.abs(delta) || delta >= 0)) { m_powerLimit += delta; }
     }
 }
